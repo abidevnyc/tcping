d9679c0abf74db16d7be0a64d1886488e25942ed
idevnyc <e@idev.nyc.mn>
2024-11-23 07:01:39 +0800

    Create tcping.c

---
+++ tcping.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <time.h>
+
+// ... 其他头文件
+
+#define BUFFER_SIZE 1024
+
+struct thread_args {
+    char *ip;
+    int port;
+    int count;
+};
+
+void *tcping_thread(void *arg) {
+    struct thread_args *args = (struct thread_args *)arg;
+    // ... 线程函数实现，包括socket创建、连接、发送、接收、计算RTT等
+}
+
+int main(int argc, char *argv[]) {
+    // ... 命令行参数解析
+    
+    // 创建线程
+    pthread_t threads[num_threads];
+    for (int i = 0; i < num_threads; i++) {
+        struct thread_args *args = malloc(sizeof(struct thread_args));
+        // ... 初始化线程参数
+        pthread_create(&threads[i], NULL, tcping_thread, (void *)args);
+    }
+    
+    // 等待所有线程结束
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    // ... 输出结果
+    return 0;
+}
